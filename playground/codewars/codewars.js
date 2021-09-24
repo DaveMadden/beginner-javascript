a7571b7182e622de8d515aa92f7a6db7b3a693c3
Dave Madden <davelearnsstuff@gmail.com>
2021-09-23 22:48:14 -0700

    triangle check, easy

--- playground/codewars/codewars.js
+++ playground/codewars/codewars.js
@@ -1,28 +1,10 @@
 console.log ("connected and shit")
 
-const test1 = "is2 Thi1s T4est 3a"
-
-function order(words){
-    let arr = words.split(" "); //break string into individual words in arrays
-    let output = [];
-    let outstr = "";
-
-    for (let i = 0; i < arr.length; i++) {
-        //find the number in the word at this indexOf
-        let num = arr[i].match(/\d+/);
-        //use that number to put this word at the right index
-        output[(num - 1)] = arr[i];
+function isTriangle(a,b,c) {
+    if ((a+b>c)&&(b+c>a)&&(a+c>b)){
+        return true;
     }
-    //turn array into a string against
-    for (let i = 0; i < arr.length; i++) {
-        if (i === 0){
-            outstr += output[i];
-        }
-        else{
-            outstr += " " + output[i];
-        }
+    else{
+        return false;
     }
-    return outstr;
-  }
-
- console.log(order(test1))
+ }
