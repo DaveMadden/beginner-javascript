3cc5caf24ec68e2fe713f506ac1cf2f42fce9958
Dave Madden <davelearnsstuff@gmail.com>
2021-09-23 22:44:30 -0700

    ordering words done

--- playground/codewars/codewars.js
+++ playground/codewars/codewars.js
@@ -5,6 +5,7 @@ const test1 = "is2 Thi1s T4est 3a"
 function order(words){
     let arr = words.split(" "); //break string into individual words in arrays
     let output = [];
+    let outstr = "";
 
     for (let i = 0; i < arr.length; i++) {
         //find the number in the word at this indexOf
@@ -12,7 +13,16 @@ function order(words){
         //use that number to put this word at the right index
         output[(num - 1)] = arr[i];
     }
-    return output;
+    //turn array into a string against
+    for (let i = 0; i < arr.length; i++) {
+        if (i === 0){
+            outstr += output[i];
+        }
+        else{
+            outstr += " " + output[i];
+        }
+    }
+    return outstr;
   }
 
  console.log(order(test1))
