ac7a3dd3b958070255f9e01906cf69005d92a83c
Dave Madden <davelearnsstuff@gmail.com>
2021-09-23 21:44:18 -0700

    working on isogram stil - cases

--- playground/codewars/codewars.js
+++ playground/codewars/codewars.js
@@ -1,6 +1,14 @@
-// console.log ("connected and shit")
+console.log ("connected and shit")
 
 function isIsogram(str){ 
-    const arr = str.split();
-    return arr;
+    const arr = str.toLowerCase().split("");
+    console.log (arr)
+    for (let i = 0; i < arr.length; i++){
+        for (let j = 0; j < arr[i].length; j++){
+            if ((i !== j) && (arr[i] === arr[j])){
+                return false
+            }
+        }
+    }
+    return true
   }
