fefae3494a72343c3a060b969fab8f7c952b5a5e
Dave Madden <davelearnsstuff@gmail.com>
2021-09-26 18:02:21 -0700

    toggle round

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -2,5 +2,13 @@
 // console.log(p)
 
 const pic = document.querySelector('.nice');
-pic.classList.toggle('round');
+// pic.classList.toggle('round');
 console.log(pic.classList);
+
+
+
+function toggleRound() {
+    pic.classList.toggle('round');
+  }
+  
+  pic.addEventListener('click', toggleRound);
