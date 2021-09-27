96f2a2e706777940ecd7e2cc84c29b6e82ab5b43
Dave Madden <davelearnsstuff@gmail.com>
2021-09-26 23:15:30 -0700

    creating with strings

--- exercises/20 - The DOM/creating.js
+++ exercises/20 - The DOM/creating.js
@@ -1,1 +1,46 @@
 console.log("worked great")
+
+const myp = document.createElement('p')
+myp.textContent = 'Hello, world'
+myp.classList.add('special')
+console.log(myp)
+
+const myImg = document.createElement('img')
+myImg.src = 'https://picsum.photos/500';
+myImg.alt = "good pic"
+console.log(myImg)
+
+const myDiv = document.createElement('div')
+myDiv.classList.add('wrapper')
+console.log(myDiv)
+
+myDiv.append(myp)
+myDiv.append(myImg)
+document.body.append(myDiv) //touch the DOM once to keep things fast
+
+const hdg = document.createElement('h2')
+hdg.textContent = "Cool Beans"
+
+myDiv.insertAdjacentElement('afterbegin', hdg)
+
+//inserting an unordered list of
+const myList = document.createElement('ul')
+
+const li1 = document.createElement('li')
+li1.textContent = "one"
+const li2 = document.createElement('li')
+li2.textContent = "two"
+const li3 = document.createElement('li')
+li3.textContent = "three"
+const li4 = document.createElement('li')
+li4.textContent = "four"
+const li5 = document.createElement('li')
+li5.textContent = "five"
+
+myList.append(li3)
+li3.insertAdjacentElement("afterbegin", li2)
+li3.insertAdjacentElement("beforeend", li4)
+myList.prepend(li1)
+myList.append(li5)
+  
+document.body.append(myList)
